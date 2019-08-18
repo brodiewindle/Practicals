f3cb0cc5825459c493cb98b87fd06159f6bf1f47
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2019-08-18 13:33:15 +1000

    Add password into functions

--- Prac_03/password_check.py
+++ Prac_03/password_check.py
@@ -1,13 +1,23 @@
-print(len(password) * '*')
+
+
+MIN_LENGTH = 2
 
 
 def main():
-    MIN_LENGTH = 2
+    password = get_password(MIN_LENGTH)
+    print_asterisks(password)
+
+def get_password(MIN_LENGTH):
 
     password = input("Please enter a password at least {} characters long: ".format(MIN_LENGTH))
 
     while len(password) < MIN_LENGTH:
+        print("Please enter a valid password!")
         password = input("Please enter a password at least {} characters long: ".format(MIN_LENGTH))
+    return password
+
+def print_asterisks(sequence):
+    print(len(sequence)*'*')
 
 
 
