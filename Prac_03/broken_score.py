2f809fe60d87db6753cc8886da330247300f018d
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2019-08-18 14:22:57 +1000

    Add score and mark teller into seperate functions

--- Prac_03/broken_score.py
+++ Prac_03/broken_score.py
@@ -3,15 +3,33 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score >= 50:
-        print("Passable")
-    if score >= 90:
-        print("Excellent")
-if score < 50:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    print(find_mark(score))
+
+
+def find_mark(score):
+
+    if 0 > score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+main()
