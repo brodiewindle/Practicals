d3207c934d37f199f0d96cc683c4be35b6cc00fb
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2019-08-27 11:39:51 +1000

    Add print statements for indexing list

--- Prac_04/list_exercises.py
+++ Prac_04/list_exercises.py
@@ -10,5 +10,11 @@ no4 = int(input("Number 4: "))
 no5 = int(input("Number 5: "))
 
 numbers = [no1, no2, no3, no4, no5]
-
 print(numbers)
+
+
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[-1]))
+print("The smallest number is {}".format(min(numbers)))
+print("The largest number is {}".format(max(numbers)))
+
