1dd7b3b6b575d4406ad7814ac9a7dc8716438bd8
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2019-08-27 11:33:00 +1000

    Add initialisation for numbers list

---
+++ Prac_04/list_exercises.py
@@ -0,0 +1,14 @@
+
+
+
+print("Please enter 5 numbers")
+
+no1 = int(input("Number 1: "))
+no2 = int(input("Number 2: "))
+no3 = int(input("Number 3: "))
+no4 = int(input("Number 4: "))
+no5 = int(input("Number 5: "))
+
+numbers = [no1, no2, no3, no4, no5]
+
+print(numbers)
