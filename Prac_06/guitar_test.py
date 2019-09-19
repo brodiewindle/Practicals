3911e910fe392b8d9d9bb54a83ce7b308a58e0d6
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2019-09-19 10:36:38 +1000

    Add checking of functions

--- Prac_06/guitar_test.py
+++ Prac_06/guitar_test.py
@@ -5,10 +5,10 @@ def main():
     gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
     another = Guitar("Another Guitar", 2012, 15000)
 
-    guitars = [gibson, another]
-    for guitar in guitars:
-        print("{} years old".format(guitar.get_age()))
-        print("{} to being vintage".format(guitar.is_vintage()))
+    print("{} is {} years old, Expected {}".format(gibson.name, gibson.get_age(), 97))
+    print("{} to being vintage - Expected {}".format(gibson.is_vintage(), True))
 
+    print("{} is {} years old, Expected {}".format(another.name, another.get_age(), 7))
+    print("{} to being vintage - Expected {}".format(another.is_vintage(), False))
 
 main()
