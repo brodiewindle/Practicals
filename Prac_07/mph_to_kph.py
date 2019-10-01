d7cb794bccda988139968880d2d11847316d6caa
brodiewindle <53396361+brodiewindle@users.noreply.github.com>
2019-10-02 09:48:55 +1000

    Add StringProperty to the program and fix minor bugs

--- Prac_07/mph_to_kph.py
+++ Prac_07/mph_to_kph.py
@@ -1,25 +1,28 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 CONVERSION_FACTOR = 1.60934
 
 
 class MphToKph(App):
+    output_display = StringProperty()
 
     def build(self):
         self.title = "MPH to KPH converter"
         self.root = Builder.load_file('mph_to_kph.kv')
         return self.root
 
-    def handle_increment(self, increment):
-        miles = int(self.root.ids.input_number.text)
-        kph = miles + increment
-        self.root.ids.output_file.text = str(kph)
+    def handle_increment(self, text, increment):
+        miles = self.str_to_float(text) + increment
+        self.root.ids.input_number.text = str(miles)
 
-    def handle_conversion(self):
-        miles = int(self.root.ids.input_number.text)
-        kph = miles * CONVERSION_FACTOR
-        self.root.output_number.text = str(kph)
+    def handle_conversion(self, text):
+        miles = self.str_to_float(text)
+        self.update_display(miles)
+
+    def update_display(self, miles):
+        self.output_display = str(miles * CONVERSION_FACTOR)
 
     @staticmethod
     def str_to_float(text):
